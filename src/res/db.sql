f7a2b6fa9343351203c39e5b3416e1f8c4a72465
Carl Holmberg <carl.j.holmberg@telia.com>
2019-09-18 00:58:30 +0200

    Added Event repository

--- src/res/db.sql
+++ src/res/db.sql
@@ -1,7 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE events (
+CREATE TABLE event (
     id uuid DEFAULT uuid_generate_v4 (),
+    name VARCHAR NOT NULLABLE,
     date DATE NOT NULLABLE,
     start_time DATE NOT NULLABLE,
     end_time DATE NOT NULLABLE,
