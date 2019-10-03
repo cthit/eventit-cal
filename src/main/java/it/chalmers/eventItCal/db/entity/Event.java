dd4b990903c6f5c9783c4ee6bf72f377167845ae
cajoho99 <31243324+cajoho99@users.noreply.github.com>
2019-10-03 23:13:48 +0200

    removed setId

--- src/main/java/it/chalmers/eventItCal/db/entity/Event.java
+++ src/main/java/it/chalmers/eventItCal/db/entity/Event.java
@@ -49,10 +49,6 @@ public class Event {
         return this.id;
     }
 
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
     public String getName() {
         return this.name;
     }
