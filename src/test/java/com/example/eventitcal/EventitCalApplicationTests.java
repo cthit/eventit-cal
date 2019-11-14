7ecd56ef8955a9e9e13676b674a48b15b87f3665
Aaron Sandgren <aaron.sandgren@live.se>
2019-11-14 21:58:04 +0100

    Spring setup redone v2

---
+++ src/test/java/com/example/eventitcal/EventitCalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eventitcal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventitCalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
